6f688b032b545d985cdfd0cf9d8fd09ba41a761b
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-09-15 16:24:21 -0700

    5

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -1,10 +1,7 @@
-/* 5-main.c */
-
 #include "main.h"
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -12,4 +9,3 @@ int main(void)
     more_numbers();
     return (0);
 }
-
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
-/* 5-more_numbers.c */
-
 #include "main.h"
 
 /**
- * more_numbers - Prints numbers from 0 to 14, 10 times.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++) // Loop 10 times
-    {
-        for (j = 0; j <= 14; j++) // Print numbers from 0 to 14
-        {
-            if (j > 9) // If the number is two digits, print the first digit
-                _putchar(j / 10 + '0');
-
-            _putchar(j % 10 + '0'); // Print the last digit
-        }
-
-        _putchar('\n'); // New line after each set of numbers
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
-
