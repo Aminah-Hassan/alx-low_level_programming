80ccf704b4b4bbf7f4a6eea4d6af9eef74b34951
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-09-15 16:26:57 -0700

    ./4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,10 +1,7 @@
-/* 4-main.c */
-
 #include "main.h"
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -12,4 +9,3 @@ int main(void)
     print_most_numbers();
     return (0);
 }
-
