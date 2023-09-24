206fffea7fb67113ad1bf07bce5463207dc1efd4
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-09-23 17:57:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	//initializing string
+	char mine[] = "Programming is like building a multilingual puzzle";
+
+	//writing to stdout
+	puts(mine);
+	return 0;
+}
