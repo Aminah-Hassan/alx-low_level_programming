7b9ab50e0025d69225333d20927090605e216d65
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-09-23 18:20:20 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual puzzle" followed by a new line.
- * return 0.
+ * main - prints Programming is like building a multilingual puzzle followed by a new line.
  */
 
 int main(void)
