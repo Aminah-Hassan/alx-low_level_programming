30ea79b13ed2989353ac8b73bf3f5ead45aa5de9
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-09-23 19:42:50 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ printf("size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-fprintf(stderr, "Anything");
+fprintf(stderr);
 
 return (0);
 }
