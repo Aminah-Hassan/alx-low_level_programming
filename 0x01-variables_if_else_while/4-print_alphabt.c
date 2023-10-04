e3448d681c165836ab0936f12658ddb0596ab9da
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-10-04 12:14:44 -0700

    exceptional letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,12 @@
 
 int main(void)
 {
-	 char C;
+	char C;
 
-	 for (C = 'a'; C <= 'z'; C++)
-	 {
-	 	if (C != 'e' && C != 'q')
-	 		putchar(C);
-	 }
-	 putchar('\n');
-	 return (0);
+	for (C = 'a'; C <= 'z'; C++) {
+		if (C != 'e' && C != 'q')
+			putchar(C);
+	}
+	putchar('\n');
+	return (0);
 }
