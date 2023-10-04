f36ffa27602c6d9af8a770e4be891bb753206e45
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-10-04 07:43:51 -0700

    printing Letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets in lower case
+ * Return: Always (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+       	for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        putchar(ch);
+	putchar('\n');
+    }
+    return (0);
+}
