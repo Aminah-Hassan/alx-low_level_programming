1c1e8a84e8a5321a4fea448e19b22d2cd9e90a30
Aminah-Hassan <meenahbila.2002@gmail.com>
2023-10-08 04:21:28 -0700

    Printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - Write a function that print_alphabet, in lowercase.
- * prints 'c'
- * Followed by a new line.
+ * print_alphabet(void) - entry point
+ * Description: This function prints the alphabet, in lowercase
  * Return: Always 0
  */
 int  putchar (int C);
